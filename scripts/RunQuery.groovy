d37dcd959683d391323249b84c406514ab63f1b1
Nathan Wells <nwwells@gmail.com>
2012-07-14 13:48:09 -0600

    Added improved parsing.

--- scripts/RunQuery.groovy
+++ scripts/RunQuery.groovy
@@ -4,27 +4,34 @@ includeTargets << grailsScript("_GrailsInit")
 includeTargets << grailsScript('_GrailsBootstrap')
 
 target(main: 'Run Hibernate HQL statement') {
-   depends(
-   // checkVersion, 
-   // configureProxy, 
-   // enableExpandoMetaClass, 
-      bootstrap //currently depending on bootstrap to get injected datasource.
-   )
+	depends(
+			// checkVersion,
+			// configureProxy,
+			// enableExpandoMetaClass,
+			bootstrap //currently depending on bootstrap to get injected datasource.
+			)
 
-   def args = argsMap.params
-   if (argsMap.file) {
-      def filename = argsMap.file
-   } else {
-      def query = args.remove(0)
-   }
-   def dataSource = appCtx.getBean('dataSource')
-   def mysqlQuery = 
+	def args
+	def query
+	if (argsMap.file) {
+		def filename = argsMap.file
+		File sqlScript = new File(argsMap.file)
+		if (sqlScript.exists() && sqlScript.canRead()) {
+			query = sqlScript.text
+			args = argsMap.params
+		} else {
+			throw new Exception("the sql script specified (${argsMap.file}), either doesn't exist," +
+								" can't be read (permissions?), or there's some other bad news.")
+		}
+	} else {
+		query = argsMap.params.remove(0)
+	}
+	def dataSource = appCtx.getBean('dataSource')
+	Sql sql = new Sql(dataSource)
+	def rows = sql.rows(query)
+	rows.each { println it }
 
-   Sql sql = new Sql(dataSource)
-   def rows = sql.rows(mysqlQuery)
-   rows.each { println it }
-
-   println argsMap
+	println argsMap
 
 }
 
