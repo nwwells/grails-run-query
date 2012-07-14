89584b14016179b35485fcec4ef216059ae685c7
Nathan Wells <nwwells@gmail.com>
2012-07-14 13:32:41 -0600

    plugin file updates and removing sweet-dreams code

--- RunQueryGrailsPlugin.groovy
+++ RunQueryGrailsPlugin.groovy
@@ -1,71 +1,63 @@
 class RunQueryGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
+	// the plugin version
+	def version = "0.1"
+	// the version or versions of Grails the plugin is designed for
+	def grailsVersion = "2.0 > *"
+	// the other plugins this plugin depends on
+	def dependsOn = [:]
+	// resources that are excluded from plugin packaging
+	def pluginExcludes = ["grails-app/views/error.gsp"]
 
-    // TODO Fill in these fields
-    def title = "Run Query Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
-    def description = '''\
-Brief summary/description of the plugin.
+	def title = "Run Query Plugin"
+	def author = "Nathan Wells"
+	def authorEmail = "nwwells+grails@gmail.com"
+	def description = '''\
+Small plugin that adds a few scripts that allow you to query your database.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/run-query"
+	// URL to the plugin's documentation
+	def documentation = "http://grails.org/plugin/run-query"
 
-    // Extra (optional) plugin metadata
+	// License: one of 'APACHE', 'GPL2', 'GPL3'
+	def license = "APACHE"
 
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+	// Any additional developers beyond the author specified above.
+	//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+	// Location of the plugin's issue tracker.
+	//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+	// Online location of the plugin's browseable source code.
+	//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
 
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+	def doWithWebDescriptor = { xml ->
+		// TODO Implement additions to web.xml (optional), this event occurs before
+	}
 
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+	def doWithSpring = {
+		// TODO Implement runtime spring config (optional)
+	}
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
+	def doWithDynamicMethods = { ctx ->
+		// TODO Implement registering dynamic methods to classes (optional)
+	}
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
+	def doWithApplicationContext = { applicationContext ->
+		// TODO Implement post initialization spring config (optional)
+	}
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
+	def onChange = { event ->
+		// TODO Implement code that is executed when any artefact that this plugin is
+		// watching is modified and reloaded. The event contains: event.source,
+		// event.application, event.manager, event.ctx, and event.plugin.
+	}
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+	def onConfigChange = { event ->
+		// TODO Implement code that is executed when the project configuration changes.
+		// The event is the same as for 'onChange'.
+	}
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+	def onShutdown = { event ->
+		// TODO Implement code that is executed when the application shuts down (optional)
+	}
 }
