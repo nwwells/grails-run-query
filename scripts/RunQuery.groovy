2ee65f73257124c4be2eb484b00284d0d6cdd4c4
Nathan Wells <nwwells@gmail.com>
2012-07-16 07:28:32 -0600

    Better error handling when parsing args

--- scripts/RunQuery.groovy
+++ scripts/RunQuery.groovy
@@ -4,35 +4,48 @@ includeTargets << grailsScript("_GrailsInit")
 includeTargets << grailsScript('_GrailsBootstrap')
 
 target(main: 'Run Hibernate HQL statement') {
-	depends(
-			// checkVersion,
-			// configureProxy,
-			// enableExpandoMetaClass,
-			bootstrap //currently depending on bootstrap to get injected datasource.
-			)
+	depends(bootstrap) //currently depending on bootstrap to get injected datasource.
 
+	def dataSource = appCtx.getBean('dataSource')
+	Sql sql = new Sql(dataSource)
 	def args
 	def query
 	if (argsMap.file) {
-		def filename = argsMap.file
-		File sqlScript = new File(argsMap.file)
-		if (sqlScript.exists() && sqlScript.canRead()) {
-			query = sqlScript.text
-			args = argsMap.params
-		} else {
-			throw new Exception("the sql script specified (${argsMap.file}), either doesn't exist," +
-								" can't be read (permissions?), or there's some other bad news.")
-		}
+		query = queryFromFile(argsMap)
+		args = argsMap.params
 	} else {
 		query = argsMap.params.remove(0)
 	}
-	def dataSource = appCtx.getBean('dataSource')
-	Sql sql = new Sql(dataSource)
-	def rows = sql.rows(query)
+	
+	def paramCount = query.count("?") 
+	def error = "The script expected $paramCount arguments and you provided ${args.size()}."
+	if (paramCount > args.size()) {
+		event "StatusError", ["- Not enough arguments: $error"]
+		System.exit(3)
+	} else if (paramCount < args.size()) {
+		event "StatusError", ["- Too many arguments: $error The argument list will be trimmed"]
+		args = args[0..<paramCount]
+	}
+	event("StatusUpdate", ["- Running query '${query}' with args $args"])
+	def rows = sql.rows(query, args)
 	rows.each { println it }
 
-	println argsMap
+}
 
+queryFromFile = { argsMap ->
+	def query = ""
+	File sqlScript = new File(argsMap.file)
+	if (!sqlScript.exists()) {
+		event("StatusError", ["- The specified query script could not be found (${sqlScript.absolutePath})"])
+		System.exit 1
+	} else if (!sqlScript.canRead()) {
+		event("StatusError", [
+			"- The current user doesn't have read access to the specified query script (${sqlScript.absolutePath})"])
+		System.exit 2
+	} else {
+		query = sqlScript.text
+	}
+	return query
 }
 
 setDefaultTarget(main)
